b022fc0332856144a06db797ee7b38378d07ea6f
GeonWoo <69147082+Y-gw@users.noreply.github.com>
2024-05-06 19:07:10 +0900

    Feat: Reader 클래스 생성

---
+++ src/main/java/LogFileReader.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+// 전체 로그 파일을 읽어와서 줄을 반환하는 클래스
+public class LogFileReader {
+    public static List<String> readLogFile(String inputFile) {
+        List<String> logLines = new ArrayList<>();                                  // logLines에 각 라인 저장 후 Arraylist로 반환
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {   // BuffererReader readLine으로 읽어오기
+            String line;
+            while ((line = br.readLine()) != null) {
+                logLines.add(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logLines;
+    }
+}
